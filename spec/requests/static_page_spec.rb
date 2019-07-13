69625a4505f1304335451a4ea6933d91e6b9caf1
watson <chikuwa.wachi@gmail.com>
2019-07-13 12:57:50 +0900

    [add]Add contract view and routing, and RSpec testcase.

--- spec/requests/static_page_spec.rb
+++ spec/requests/static_page_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "StaticPages", type: :request do
+RSpec.describe "[StaticPagesコントローラ]", type: :request do
   # 期待値となるtitleタグの文字列を定義する
   let(:base_title) { " | Ruby on Rails Tutorial Sample App" }
 
@@ -8,18 +8,35 @@ RSpec.describe "StaticPages", type: :request do
     "home",
     "help",
     "about",
+    "contact",
   ]
 
   action_methods.each do |action_method|
-    it "[#{action_method}]GETメソッドでリクエストを送り、200が返ってくることと、titleタグが想定通りの値となっていることを確認する" do
-      get "/static_pages/#{action_method}"
-
+    context "[#{action_method}アクションのViewレスポンスが想定どおりであること]" do
       # 200ステータスが返ってくること
-      expect(response).to have_http_status "200"
+      it "HTTPレスポンス=200" do
+        get "/static_pages/#{action_method}"
+        expect(response).to have_http_status "200"
+      end
 
-      # titleタグが想定した内容であること
       action_method_camel = action_method.camelize
-      expect(response.body).to match(/(.+)<title>#{action_method_camel + base_title}<\/title>(.+)/)
+
+      it "titleが想定通り" do
+        get "/static_pages/#{action_method}"
+        # Homeは特定の名称を使用、Home以外はアクション名(先頭大文字)を採用
+        expect(response.body).to match(/(.+)<title>#{action_method_camel + base_title}<\/title>(.+)/)
+      end
+      it "h1タグが想定通り" do
+        get "/static_pages/#{action_method}"
+        # Homeは特定の名称を使用、Home以外はアクション名(先頭大文字)を採用
+        reg_str = ""
+        if action_method == "home"
+          reg_str = "Sample App"
+        else
+          reg_str = action_method.camelize
+        end
+        expect(response.body).to match(/(.*)<h1>#{reg_str}<\/h1>(.*)/)
+      end
     end
   end
 end
