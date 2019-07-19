25f2bc2277256f837065206b54c17e04ca991c71
watson <chikuwa.wachi@gmail.com>
2019-07-19 21:13:02 +0900

    [change]Completed section 4.1.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # 各ページのtitileを生成する
+  def full_title(page_title = "")
+    base_tiltle = "Ruby on Rails Tutorial Sample App"
+
+    # ページ名が存在しない場合は「|」を表示しない
+    if page_title.empty?
+      base_tiltle
+    else
+      "#{page_title} | #{base_tiltle}"
+    end
+  end
 end
