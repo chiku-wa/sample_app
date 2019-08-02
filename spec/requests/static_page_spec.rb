84e067ef279010efd623820095f60ad86dc0ae10
watson <chikuwa.wachi@gmail.com>
2019-08-02 14:09:10 +0900

    [add, change]Complete section 5.1.2, and delete Unnecessary test.

--- spec/requests/static_page_spec.rb
+++ spec/requests/static_page_spec.rb
@@ -25,26 +25,6 @@ RSpec.describe "StaticPagesController-requests", type: :request do
         get "/static_pages/#{action_method}"
         expect(response).to have_http_status "200"
       end
-
-      action_method_camel = action_method.camelize
-
-      it "titleが想定通り" do
-        get "/static_pages/#{action_method}"
-        # Homeは特定の名称を使用、Home以外はアクション名(先頭大文字)を採用
-        expect(response.body).to match(/(.+)<title>#{action_method_camel + base_title}<\/title>(.+)/)
-      end
-      it "h1タグが想定通り" do
-        get "/static_pages/#{action_method}"
-
-        # Homeは特定の名称を使用、Home以外はアクション名(先頭大文字)を採用
-        reg_str = ""
-        if action_method == "home"
-          reg_str = "Sample App"
-        else
-          reg_str = action_method.camelize
-        end
-        expect(response.body).to match(/(.*)<h1>#{reg_str}<\/h1>(.*)/)
-      end
     end
   end
 end
