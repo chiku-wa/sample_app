66f7cdb29af114ce3db6c5d939dcdc6513795da6
watson <chikuwa.wachi@gmail.com>
2019-08-14 08:30:49 +0900

    [change]Modify logo link, and add test case.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,15 +1,39 @@
 require "rails_helper"
 
 RSpec.feature "StaticPages", type: :feature do
-  scenario "linls root and Home" do
-    # ルートにアクセスする
-    visit root_path
+  feature "Headerにあるリンクが正常に機能すること" do
+    scenario "Homeリンク(root_path)" do
+      visit root_path
+      click_link "Home"
+      expect(page).to have_current_path root_path
+    end
 
-    # 指定したinner HTMLのリンクが存在することを確認する
-    # クリックするだけで
-    click_link "Home"
+    scenario "Aboutリンク" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_current_path about_path
+    end
 
-    # 遷移したページが想定どおりであることを確認する
-    expect(page).to have_current_path root_path
+    scenario "Helperリンク" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_current_path about_path
+    end
+  end
+
+  feature "aタグのラベルとリンクが想定どおりであること" do
+    scenario "sample app, Home-root_path" do
+      visit root_path
+      expect(page).to(have_link "sample app", href: root_path, count: 1)
+      expect(page).to(have_link "Home", href: root_path, count: 1)
+    end
+    scenario "About-about_path" do
+      visit root_path
+      expect(page).to(have_link "About", href: about_path, count: 1)
+    end
+    scenario "Help-help_path" do
+      visit root_path
+      expect(page).to(have_link "Help", href: help_path, count: 1)
+    end
   end
 end
