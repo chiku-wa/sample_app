0bc1ccc7be96ab923ea1f63e08d2dd9cd8aaea9c
watson <chikuwa.wachi@gmail.com>
2019-08-21 18:45:56 +0900

    [change]Completed section 5.4, and fix test case for enabled "exact: true".

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -7,48 +7,71 @@ RSpec.feature "StaticPages", type: :feature do
   feature "Headerにあるリンクが正常に機能すること" do
     scenario "Homeリンク(root_path)が正常に機能すること" do
       visit root_path
-      expect(page).to(have_link "sample app", href: root_path, count: 1)
-      expect(page).to(have_link "Home", href: root_path, count: 1)
+      expect(page).to(
+        have_link("sample app", href: root_path, count: 1)
+      )
+      expect(page).to(
+        have_link("Home", href: root_path, count: 1)
+      )
 
       click_link "Home"
-      expect(page).to(have_current_path root_path)
-      expect(page).to(have_title full_title)
+      expect(page).to(
+        have_current_path(root_path)
+      )
+      expect(page).to(
+        have_title(full_title, exact: true)
+      )
     end
 
     scenario "Helpリンクが正常に機能すること" do
       visit root_path
-      expect(page).to(have_link "Help", href: help_path, count: 1)
+      expect(page).to(
+        have_link("Help", href: help_path, count: 1)
+      )
 
       click_link "Help"
-      expect(page).to(have_current_path help_path)
-      expect(page).to(have_title full_title("Help"))
+      expect(page).to(
+        have_current_path((help_path))
+      )
+      expect(page).to(
+        have_title(full_title("Help"), exact: true)
+      )
     end
 
     # Fixme
     # Log in機能を実装したらテストケースを修正する
     scenario "Log inリンク" do
       visit root_path
-      expect(page).to(have_link "Log in", href: "#", count: 1)
+      expect(page).to(
+        have_link("Log in", href: "#", count: 1)
+      )
 
       click_link "Log in"
-      expect(page).to(have_current_path root_path)
-      # expect(page).to(have_title full_title("XXX"))
+      expect(page).to(
+        have_current_path(root_path)
+      )
+      # expect(page).to(have_title(full_title("XXX"),exact:true))
     end
   end
 
   # ---------------
   # yieldに関するテスト
   # ---------------
-  feature "Homeアクションのビューのリンクが正常に機能すること" do
+  feature "yieldアクションのビューのリンクが正常に機能すること" do
     scenario "Singn up now!リンク" do
       visit root_path
-      expect(page).to(have_link, href: signup_path, count: 1)
+      expect(page).to(
+        have_link(href: signup_path, count: 1)
+      )
 
       click_link "Sign up now!"
-      expect(page).to(have_current_path signup_path)
-      # Fixme
-      # Users#newのViewが完成次第テストケースを修正する
-      expect(page).to(have_title full_title)
+      expect(page).to(
+        have_current_path(signup_path)
+      )
+
+      expect(page).to(
+        have_title(full_title("Sign up"), exact: true)
+      )
     end
   end
 
@@ -58,20 +81,32 @@ RSpec.feature "StaticPages", type: :feature do
   feature "Footerにあるリンクが正常に機能すること" do
     scenario "Aboutリンクが正常に機能すること" do
       visit root_path
-      expect(page).to(have_link "About", href: about_path, count: 1)
+      expect(page).to(
+        have_link("About", href: about_path, count: 1)
+      )
 
       click_link "About"
-      expect(page).to(have_current_path about_path)
-      expect(page).to(have_title full_title("About"))
+      expect(page).to(
+        have_current_path(about_path)
+      )
+      expect(page).to(
+        have_title(full_title("About"), exact: true)
+      )
     end
 
     scenario "Contactリンクが正常に機能すること" do
       visit root_path
-      expect(page).to(have_link "Contact", href: contact_path, count: 1)
+      expect(page).to(
+        have_link("Contact", href: contact_path, count: 1)
+      )
 
       click_link "Contact"
-      expect(page).to(have_current_path contact_path)
-      expect(page).to(have_title full_title("Contact"))
+      expect(page).to(
+        have_current_path(contact_path)
+      )
+      expect(page).to(
+        have_title(full_title("Contact"), exact: true)
+      )
     end
 
     # 「News」は外部リンクなのでテストしない
