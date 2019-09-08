086900fd15b7c09ff33d945f823d09efaa5c6050
watson <chikuwa.wachi@gmail.com>
2019-09-08 09:06:53 +0900

    [add,change]Completed section 7.3.3.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # User登録が完了した場合の処理をここに書く
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
