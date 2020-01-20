9e7fda5779aec4b5346f0dda6ff9c71bd4d8e4b3
watson <chikuwa.wachi@gmail.com>
2020-01-20 09:14:30 +0900

    [change]Change rails generate setting for test_framework.

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,4 +2,9 @@ Rails.application.config.generators do |g|
   g.stylesheets false
   g.javascripts false
   g.helper false
+  g.test_framework :rspec, {
+    view_specs: false,
+    helper_specs: false,
+    controller_specs: false,
+  }
 end
