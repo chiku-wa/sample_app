be5786a94e6780a603c863fce97ec9b719e29578
watson <chikuwa.wachi@gmail.com>
2020-04-09 08:35:40 +0900

    [fix,change]Add feature test for image upload, and change micropost form.

--- spec/features/microposts_spec.rb
+++ spec/features/microposts_spec.rb
@@ -4,8 +4,19 @@ RSpec.feature "Microposts", type: :feature do
   # 投稿フォームの有無を確認するためのxpath
   let(:xpath_post_form) { "//textarea[@id='micropost_content']" }
 
+  # マイクロポスト一覧の画像の有無を確認するためのxpath
+  let(:xpath_micropost_image) { "//li/span[@class='content']/img" }
+
   # マイクロポスト一覧の有無を確認するためのxpath
   let(:xpath_micropost_list) { "//ol[@class='microposts']" }
+
+  # ===== 予め用意したテスト用画像の情報を定義
+  # ディレクトリ
+  let(:test_image_path) { File.join(Rails.root, "spec/fixtures") }
+  # 正常系テスト用の画像ファイル名を定義
+  let(:jpg_file_name) { "sample.jpg" }
+  # 異常系テスト用の画像ファイル名を定義
+  let(:bmp_file_name) { "sample.bmp" }
 
   before "テストユーザ登録" do
     # --- マイクロソフトの取得順序テスト用のデータ
@@ -65,22 +76,75 @@ RSpec.feature "Microposts", type: :feature do
       expect(page).to(have_xpath(xpath_micropost_list, count: 1))
     end
 
-    scenario "有効なマイクロポストを投稿する" do
+    scenario "本文のみでマイクロポストを正常に投稿できること" do
       login_operation(@user)
 
-      operation_post_micropost("a" * 140)
+      expect {
+        operation_post_micropost("a" * 140)
+      }.to change(Micropost, :count).by(1)
 
+      # 成功メッセージが表示されること
       expect(page).to have_selector(
         ".alert.alert-success",
         text: "Micropost created!",
       )
     end
 
-    scenario "無効なマイクロポストを投稿する" do
+    scenario "許可された拡張子のファイルで正常に投稿できること" do
+      login_operation(@user)
+
+      expect {
+        operation_post_micropost(
+          "Image upload test.",
+          "#{test_image_path}/#{jpg_file_name}",
+        )
+      }.to change(Micropost, :count).by(1)
+
+      # マイクロポスト一覧の先頭に想定する画像がアップロードされていること
+      # img要素のsrc属性をテストする
+      expect(page).to(have_title(full_title))
+      expect(
+        page.all(xpath_micropost_image)[0][:src]
+      ).to(have_content(jpg_file_name))
+
+      # 成功メッセージが表示されること
+      expect(page).to have_selector(
+        ".alert.alert-success",
+        text: "Micropost created!",
+      )
+    end
+
+    scenario "許可されていない拡張子のファイルの場合は投稿できないこと" do
+      login_operation(@user)
+
+      expect {
+        operation_post_micropost(
+          "Image upload test.",
+          "#{test_image_path}/#{bmp_file_name}",
+        )
+      }.to change(Micropost, :count).by(0)
+
+      # マイクロポスト一覧img要素を持つマイクロポストが存在しないこと
+      expect(page).to(have_title(full_title))
+      expect(page.all(xpath_micropost_image)).not_to(have_content(bmp_file_name))
+
+      # 失敗メッセージが表示されること
+      expect(page).to have_selector(
+        ".alert.alert-danger",
+        text: "The form contains 1 error",
+      )
+      expect(page).to have_text(
+        'Picture You are not allowed to upload "bmp" files, allowed types: jpg, jpeg, gif, png',
+      )
+    end
+
+    scenario "無効な本文ではマイクロポストを投稿できないこと" do
       login_operation(@user)
 
       # 上限を超えた文字数を投稿する
-      operation_post_micropost("a" * 141)
+      expect {
+        operation_post_micropost("a" * 141)
+      }.to change(Micropost, :count).by(0)
 
       # エラー数が出力されていること
       expect(page).to(have_content(/The form contains 1 error*/))
@@ -88,7 +152,7 @@ RSpec.feature "Microposts", type: :feature do
       expect(page).to(have_content("Content is too long (maximum is 140 characters)", count: 1))
     end
 
-    scenario "投稿後は、マイクロポスト一覧の先頭に投稿内容が表示されていること" do
+    scenario "正常に投稿できた後は、マイクロポスト一覧の先頭に投稿内容が表示されていること" do
       login_operation(@user)
 
       # 投稿内容を定義
@@ -96,10 +160,9 @@ RSpec.feature "Microposts", type: :feature do
 
       operation_post_micropost(post_content)
 
-      # 期待値確認用に、マイクロポストの本文をXPathとして定義
+      # TOP画面のマイクロポスト一覧の先頭に表示されていること
       xpath_micropost_content = "//li/span[@class='content']"
 
-      # TOP画面のマイクロポスト一覧の先頭に表示されていること
       expect(page).to(have_title(full_title))
       expect(page.all(xpath_micropost_content)[0]).to(have_content(post_content))
 
@@ -118,7 +181,9 @@ RSpec.feature "Microposts", type: :feature do
       before_micropost_size = all(:xpath, xpath_micropost_content).size
 
       visit root_url
-      all(:xpath, "//li/span[@class='timestamp']/a[@data-method='delete']")[1].click
+      expect {
+        all(:xpath, "//li/span[@class='timestamp']/a[@data-method='delete']")[1].click
+      }.to change(Micropost, :count).by(-1)
 
       after_micropost_size = all(:xpath, xpath_micropost_content).size
 
@@ -145,13 +210,20 @@ RSpec.feature "Microposts", type: :feature do
   # ======================================
   private
 
-  # マイクロポストを投稿する操作
-  def operation_post_micropost(post_content)
+  # 本文のみでマイクロポストを投稿する操作
+  # 第2引数の画像がnilの場合は画像の添付は行わない
+  def operation_post_micropost(post_content, image_path = nil)
     # 投稿画面を表示
     visit root_path
 
-    # 投稿する
+    # 投稿内容を入力
     fill_in("micropost_content", with: post_content)
+    if image_path
+      find(:xpath, "//*[@id='micropost_picture']").click
+      attach_file(image_path)
+    end
+
+    # 投稿する
     click_button("Post")
   end
 end
