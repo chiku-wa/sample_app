03f2a1246b2802690a1fee365ebe522ca560c2d8
watson <chikuwa.wachi@gmail.com>
2020-04-25 07:35:49 +0900

    [improve]Change comment.

--- spec/models/follower_followed_spec.rb
+++ spec/models/follower_followed_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "FollowerFollowedモデルのテスト", type: :model do
   context "従属関係のテスト" do
     it "フォワー、フォローされているユーザが取得できること(従属関係のテスト)" do
-      # テストユーザ：フォロワーユーザ
+      # フォロワーユーザ
       follower_user = User.new(
         name: "Cacy",
         email: "Cacy@example.com",
@@ -12,7 +12,7 @@ RSpec.describe "FollowerFollowedモデルのテスト", type: :model do
       )
       follower_user.save
 
-      # テストユーザ：フォローされるユーザ
+      # フォローされるユーザ
       followed_user = User.new(
         name: "Alice",
         email: "Alice@example.com",
@@ -25,10 +25,10 @@ RSpec.describe "FollowerFollowedモデルのテスト", type: :model do
       follower_user.followeds.build(followed_id: followed_user.id)
       follower_user.save
 
-      # フォロワー一覧、フォローされているユーザが取得できること
-      ff = FollowerFollowed.first
-      expect(ff.follower).to eq follower_user
-      expect(ff.followed).to eq followed_user
+      # フォロワー、フォローされているユーザが取得できること
+      follower_followed = FollowerFollowed.first
+      expect(follower_followed.follower).to eq follower_user
+      expect(follower_followed.followed).to eq followed_user
     end
   end
 end
