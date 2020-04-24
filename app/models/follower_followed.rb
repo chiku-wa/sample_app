a5980a1ce730a803d586b9c86e265fe5ba5b82b1
watson <chikuwa.wachi@gmail.com>
2020-04-25 08:19:29 +0900

    [improve]Add unique validation to FollowerFollowed class.

--- app/models/follower_followed.rb
+++ app/models/follower_followed.rb
@@ -2,4 +2,12 @@ class FollowerFollowed < ApplicationRecord
   # === 従属関係
   belongs_to(:follower, { class_name: "User", foreign_key: "follower_id" })
   belongs_to(:followed, { class_name: "User", foreign_key: "followed_id" })
+
+  # === バリデーション
+  validates(
+    :follower,
+    {
+      uniqueness: { scope: :followed },
+    },
+  )
 end
--- spec/models/follower_followed_spec.rb
+++ spec/models/follower_followed_spec.rb
@@ -1,34 +1,59 @@
 require "rails_helper"
 
 RSpec.describe "FollowerFollowedモデルのテスト", type: :model do
+  before do
+    # フォロワーユーザ
+    @follower_user = FactoryBot.build(:follower_user)
+    @follower_user.save
+
+    # フォローされるユーザ
+    @followed_user = FactoryBot.build(:followed_user)
+    @followed_user.save
+  end
+
+  context "バリデーションのテスト" do
+    # --- follower_id,followed_idのテスト
+    it "同じfollower_id,followed_idの組み合わせを持つレコードの場合はバリデーションエラーとなること" do
+      # バリエーションテストのためのユーザを登録
+      follower_user_second = FactoryBot.build(:follower_user_second)
+      follower_user_second.save
+      followed_user_second = FactoryBot.build(:followed_user_second)
+      followed_user_second.save
+
+      # フォローする
+      @follower_user.followeds.build(followed_id: @followed_user.id)
+      @follower_user.save
+      follower_user_second.followeds.build(followed_id: followed_user_second.id)
+      follower_user_second.save
+
+      # 同じfollower_id,followed_idの組み合わせの場合はエラーとなること
+      expect(
+        @follower_user.followeds.build(followed_id: @followed_user.id)
+      ).not_to be_valid
+      expect(
+        follower_user_second.followeds.build(followed_id: followed_user_second.id)
+      ).not_to be_valid
+
+      # 異なる組み合わせの同じfollower_id,followed_idの場合はエラーとならないこと
+      expect(
+        @follower_user.followeds.build(followed_id: followed_user_second.id)
+      ).to be_valid
+      expect(
+        follower_user_second.followeds.build(followed_id: @followed_user.id)
+      ).to be_valid
+    end
+  end
+
   context "従属関係のテスト" do
     it "フォワー、フォローされているユーザが取得できること(従属関係のテスト)" do
-      # フォロワーユーザ
-      follower_user = User.new(
-        name: "Cacy",
-        email: "Cacy@example.com",
-        password: "foobar",
-        password_confirmation: "foobar",
-      )
-      follower_user.save
-
-      # フォローされるユーザ
-      followed_user = User.new(
-        name: "Alice",
-        email: "Alice@example.com",
-        password: "foobar",
-        password_confirmation: "foobar",
-      )
-      followed_user.save
-
       # ユーザをフォローする
-      follower_user.followeds.build(followed_id: followed_user.id)
-      follower_user.save
+      @follower_user.followeds.build(followed_id: @followed_user.id)
+      @follower_user.save
 
       # フォロワー、フォローされているユーザが取得できること
       follower_followed = FollowerFollowed.first
-      expect(follower_followed.follower).to eq follower_user
-      expect(follower_followed.followed).to eq followed_user
+      expect(follower_followed.follower).to eq @follower_user
+      expect(follower_followed.followed).to eq @followed_user
     end
   end
 end
