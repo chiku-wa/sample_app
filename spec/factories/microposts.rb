da6af36ea3d9f69539bc7e64d9c111bc283360e1
watson <chikuwa.wachi@gmail.com>
2020-06-07 12:25:37 +0900

    [change]Add feature test case for feed to home page.

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -9,6 +9,11 @@ FactoryBot.define do
     created_at { latest_time }
   end
 
+  factory :micropost_5min_ago, class: Micropost do
+    content { "Test message 5分前" }
+    created_at { latest_time.ago(5.minutes) }
+  end
+
   factory :micropost_10min_ago, class: Micropost do
     content { "Test message 10分前" }
     created_at { latest_time.ago(10.minutes) }
@@ -22,5 +27,10 @@ FactoryBot.define do
   factory :micropost_3years_ago, class: Micropost do
     content { "Test message 3年前" }
     created_at { latest_time.ago(3.years) }
+  end
+
+  factory :micropost_5years_ago, class: Micropost do
+    content { "Test message 5年前" }
+    created_at { latest_time.ago(5.years) }
   end
 end
